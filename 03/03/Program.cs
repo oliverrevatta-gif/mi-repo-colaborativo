87cf5c5577a07d18f000aba3077c2c35c7e73c70
oliverrevatta-gif <oliverrevatta@gmail.com>
2026-05-07 12:13:27 -0500

    Add files via upload

---
+++ 03/03/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int stock = 500;
+            int totalVendido = 0;
+
+            Console.Write("Unidades vendidas (0 para salir): ");
+            int venta = int.Parse(Console.ReadLine());
+
+            
+            while (venta != 0)               
+            {
+                
+                if (venta < 0)             
+                {
+                    Console.WriteLine("Error: cantidad no puede ser negativa.");
+                }
+                
+                else if (venta > stock)     
+                {
+                    Console.WriteLine($"Error: stock insuficiente. Quedan {stock}");
+                }
+                else
+                {
+                   
+                    stock = stock - venta;        
+
+                    
+                    totalVendido = totalVendido + venta; 
+                    Console.WriteLine($"Venta registrada. Stock: {stock}");
+                }
+
+                
+                Console.Write("Unidades vendidas (0 para salir): ");
+                venta = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Stock restante: {stock}");
+            Console.WriteLine($"Total vendido: {totalVendido}");
+        }
+    }
+}
